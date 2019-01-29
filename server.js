b6d09b51a3c0e4ba68fd184035e91f42052da446
jrmccann2 <jshmccann@gmail.com>
2019-01-29 12:23:57 -0700

    added dependencies

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static('./'))
 
 
+
+app.post("/api/contact", (req, res) => {
+    console.log(req.body)
+    res.sendStatus(200)
+})
 
 app.listen(4545, () => {
     console.log("Listening on port 4545")
